799e95370d7e60a57877b76a17f765f9118d1227
bhupeshve <146075582+bhupeshve@users.noreply.github.com>
2023-10-11 20:42:25 +0200

    Update calc_intensity_inside.m

--- calc_intensity_inside.m
+++ calc_intensity_inside.m
@@ -4,28 +4,14 @@
 
 function [result_innermost] = calc_intensity_inside(orig_image, boundary, excelFile, j)
 
-  
     %%% This part of the code deals with ROI
     % Create a binary mask of the ROI INSIDE the offset polygon
     roiMaskInside = poly2mask(boundary.Vertices(:, 1), boundary.Vertices(:, 2), size(orig_image, 1), size(orig_image, 2));
     % Compute the area of the ROI using regionprops
     stats = regionprops(roiMaskInside, 'Area'); % stats.Area gives the area in pixels of ROI
 
-
     % Extract the pixels within the ROI from the grayscale image
     roiPixels = orig_image(roiMaskInside);
-    
-    %%%%%% Assign a colormap for visualizing the ROI
-    coloredImage = orig_image;
-    coloredImage(roiMaskInside) = 200; % Assign a color (white) to the ROI pixels
-    
-    %%%%Display the grayscale image with the ROI overlay
-    figure();
-    imshow(coloredImage);
-    hold on;
-    plot(boundary, 'FaceColor', 'none', 'EdgeColor', 'r', 'LineWidth', 2);
-
-    title('Grayscale original Image with ROI Overlay for innermost/center region');
     
     % Compute intensity statistics (e.g., mean and median) for the ROI
     meanIntensity = mean(roiPixels);
