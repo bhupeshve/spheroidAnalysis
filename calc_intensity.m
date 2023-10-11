bbf0ef2e94dc95dadb3b949f858ee5dbedeae058
bhupeshve <146075582+bhupeshve@users.noreply.github.com>
2023-10-11 20:42:05 +0200

    Update calc_intensity.m

--- calc_intensity.m
+++ calc_intensity.m
@@ -19,21 +19,7 @@ function [boundary, intensity_inside, intensity_between, result_inside] = calc_i
     % Buffer (expand) or erode (shrink) the polyshape
     offsetPolygon = polybuffer(offsetPolygon, offset, 'JointType', 'round');
     boundary = offsetPolygon;
-
-    %%%%%Display the binary image
-    % figure();
-    % title('Convex hull and offset cuve')
-    % imshow(orig_image);
-    % hold on; % Allow superimposing the boundary on the image
-    % 
-    % %%%%%plot the polygon
-    % plot(convexHullY, convexHullX, 'r', 'LineWidth', 2);
-    % hold on;
-    % % Plot the offset polygon in red
-    % plot(offsetPolygon, 'FaceColor', 'none', 'EdgeColor', 'r', 'LineWidth', 2);
-
-    % hold off; % Release the hold on the image
-    
+   
     
     %%% This part of the code deals with ROI
     % Create a binary mask of the ROI INSIDE the offset polygon
@@ -45,15 +31,6 @@ function [boundary, intensity_inside, intensity_between, result_inside] = calc_i
     % Extract the pixels within the ROI from the grayscale image
     roiPixels = orig_image(roiMaskInside);
     
-    % Assign a colormap for visualizing the ROI
-    % coloredImage = orig_image;
-    % coloredImage(roiMaskInside) = 200; % Assign a color (white) to the ROI pixels
-    
-    %%%%%Display the grayscale image with the ROI overlay
-    % figure();
-    % imshow(coloredImage);
-    % title('Grayscale original Image with ROI Overlay inside offset curve');
-    
     % Compute intensity statistics (e.g., mean and median) for the ROI
     meanIntensity = mean(roiPixels);
     
@@ -63,15 +40,6 @@ function [boundary, intensity_inside, intensity_between, result_inside] = calc_i
         intensity_inside = 0; return;
     end
 
-    medianIntensity = median(roiPixels);
-    result_inside = {offset_length*k, stats.Area,sum(roiPixels),mean(roiPixels),median(roiPixels),std(double(roiPixels)), max(roiPixels)};
-    location = strcat('A', num2str(k+1));
-    %%%%writecell(result_inside, excelFile, 'Sheet', 'Inside offset Curve','Range', location);
-
-    % %%%Display the computed intensity statistics
-    fprintf('Mean Intensity within the offset cuve for offset value of %.2f μm is: %.2f\n', offset_length*k, meanIntensity);
-    fprintf('Median Intensity within the offset curve: %.2f\n', medianIntensity);
-    
     %%% Now here I want to compute the intensity BETWEEN the convex hull and
     %%% offset curve
     % Invert the binary mask that represents the convex hull
@@ -79,16 +47,6 @@ function [boundary, intensity_inside, intensity_between, result_inside] = calc_i
        
     % Combine the two masks using the logical AND operation to get the pixels between offset curve and convex hull
     roiMaskBetweenOffsetAndHull = ~roiMaskInside & convexHullMask;%roiMaskBetweenHullAndOffset;
-    
-    % Assign a colormap for visualizing the ROI
-    % figure();
-    % coloredImage = orig_image;
-    % coloredImage(roiMaskBetweenOffsetAndHull) = 200; % Assign a color (white) to the ROI pixels
-    
-    %%%Display the grayscale image with the ROI overlay
-    % figure();
-    % imshow(coloredImage);
-    % title('Grayscale Image with ROI Overlay between offset curve and convex hull');
     
     % Extract the pixels within this ROI from the grayscale image
     roiPixelsBetweenOffsetAndHull = orig_image(roiMaskBetweenOffsetAndHull);
